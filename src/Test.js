d03079ccf993f9d837c904adecb54393bf3cd42b
ahnyohan <ayh0912@gmail.com>
2019-04-29 22:16:28 +0900

    test1

---
+++ src/Test.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Test extends Component{
+    render(){
+        return (
+            <div>
+                123123123123
+
+            </div>
+        );
+    }
+}
+
+export default Test;
